889223a25611c729566f086509e3fd7ab1aa2644
Dhanvi-Bhimani <dhanvi.bhimani@ics-global.in>
2025-02-06 17:54:31 +0530

    UserForm class added

--- app/forms.py
+++ app/forms.py
@@ -20,3 +20,19 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        Length(min=6),
+        EqualTo('confirm_password', message='Passwords must match')
+    ])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    role = SelectField('Role', coerce=int, validators=[DataRequired()])  # Role dropdown
+    submit = SubmitField('Submit')
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.all()]
