e3f88932d65860887bd3441ea4c9dfd44c61a078
Dhanvi-Bhimani <dhanvi.bhimani@ics-global.in>
2025-02-07 13:07:58 +0530

    changes done

--- insert_role.py
+++ insert_role.py
@@ -1,13 +1,21 @@
-from app import db, create_app
-from app.models import Role
+# from app import db, create_app
+# from app.models import Role
 
-app = create_app()
+# app = create_app()
 
-with app.app_context():
-    roles = ["Admin", "Doctor", "Patient", "Nurse", "Receptionist"]
-    for role_name in roles:
-        if not Role.query.filter_by(name=role_name).first():
-            db.session.add(Role(name=role_name))
+# with app.app_context():
+#     roles = ["Admin", "Doctor", "Patient", "Nurse", "Receptionist"]
+#     for role_name in roles:
+#         if not Role.query.filter_by(name=role_name).first():
+#             db.session.add(Role(name=role_name))
     
-    db.session.commit()
-    print("Roles added successfully!")
+#     db.session.commit()
+#     print("Roles added successfully!")
+    
+# from app import db, create_app
+
+# app = create_app()
+# with app.app_context():
+#     db.create_all()
+#     print("Database created successfully!")
+
